9efd50f60ec33bc191f20fb2bdba9012342b9967
Nikita Nadymov <36726523+Nirkaz@users.noreply.github.com>
2023-01-27 18:44:34 +0400

    Created folder for view models. Created BugDetails view model.

---
+++ BugTracker/ViewModels/BugDetailsViewModel.cs
@@ -0,0 +1,11 @@
+﻿using BugTracker.Models;
+using System.Collections;
+
+namespace BugTracker.ViewModels
+{
+    public class BugDetailsViewModel
+    {
+        public Bug Bug { get; set; }
+        public List<Comment> Comments { get; set; }
+    }
+}
--- BugTracker/ViewModels/ErrorViewModel.cs
+++ BugTracker/ViewModels/ErrorViewModel.cs
@@ -1,4 +1,4 @@
-namespace BugTracker.Models
+namespace BugTracker.ViewModels
 {
     public class ErrorViewModel
     {
